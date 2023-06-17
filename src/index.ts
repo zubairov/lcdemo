0a0359fd6267aa5ba4d6af8d4377c7af3be4c51f
Renat Zubayrov <56208+zubairov@users.noreply.github.com>
2023-06-17 21:42:39 +0200

    Added gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+import { PromptTemplate } from "langchain/prompts";
 import { OpenAI } from "langchain/llms";
 
 const model = new OpenAI({
   temperature: 0.1,
 });
 
-const res = await model.call("What is the capital of Germany?");
-console.log("Hello world!", res);
+const template = "What is the capital of {country}?";
+const prompt = new PromptTemplate({
+  template: template,
+  inputVariables: ["country"],
+});
+// const res = await model.call("What is the capital of Germany?");
+console.log("Hello world!", prompt.format({ country: "Grece" }));
